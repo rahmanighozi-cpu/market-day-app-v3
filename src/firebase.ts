074e243a02d423f19350e54a96d643a9d2b75f52
rahmanighozi-cpu <rahmanighozi@gmail.com>
2026-06-05 22:35:03 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,15 +3,17 @@ import { getDatabase } from "firebase/database";
 
 // Konfigurasi Firebase asli milik database Anda
 const firebaseConfig = {
-  apiKey: "AIzaSy", // Ini biarkan default atau isi dari config web Firebase Anda jika ada
+  apiKey: "AIzaSyDD8-ZJU3rPgfjqymahRkEiEGzqiW5LnOQ",
   authDomain: "market-day-app.firebaseapp.com",
   databaseURL: "https://market-day-app-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "market-day-app",
-  storageBucket: "market-day-app.appspot.com",
+  storageBucket: "market-day-app.firebasestorage.app",
+  messagingSenderId: "745951785761",
+  appId: "1:745951785761:web:6bf856c49cea16ba438460"
 };
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 
-// Ekspor database agar bisa langsung dipakai di file lain
+// Ekspor database agar bisa langsung dipakai di file store.ts
 export const db = getDatabase(app);
